99dfc46bfdc947f6072003daa89df3266c756d23
omnisonic <omnisonic@gmail.com>
2022-07-07 14:15:57 -0700

    readme

---
+++ readme.py
@@ -0,0 +1,18 @@
+* under developement.  A memory aid project.
+Flashcard application  using python.
+Cards are devided into different decks depending on their learned status by  the user.  One deck has all the cards.  Another deck is of the current working set of cards.  The third deck is the cards that the user has sufficiently studied for their current session.
+
+The strategy is to start with two cards and add cards incrementally to the current study deck.
+
+In this way the memorization starts easily and your memory builds upon and references what it already knows in order to remember more.
+
+Once a card feels easy to recall , the user move it into the third "archive deck.
+
+Todo: adapt the program to accept different types of data structures from diffent surces.  (a good start is to tap into and api / json data)
+
+Todo: GUI first with python, then with javascript/React
+
+
+Todo: save progress longterm functionality.  import module for saving to file.
+
+Todo: ...
